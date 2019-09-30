a76c2f28fb387349b46d5d71f6436cffd7c9995c
Robert Lee <bobbydwise1@hotmail.com>
2019-09-29 20:19:59 -0700

    Create bussiness logic solution.

--- scripts.js
+++ scripts.js
@@ -8,6 +8,33 @@ For example, given the array [10, 5, 7], you should return true, since we can mo
 Given the array [10, 5, 1], you should return false, since we can't modify any one element to get a non-decreasing array.
 */
 
+const makeDec = (yourArray) => {
+  if (yourArray.length < 3) {
+    return null
+  }
+  let count = 0
+  for ( let i = 0; i < yourArray.length-1; i++ ) {
+    if ( yourArray[i] > yourArray[i+1] ) {
+      count++
+    }
+  }
+  if ( count <= 1 ) {
+    return true;
+  } else if (count > 1) {
+    return false;
+  }
+}
+
+let test1 = [10,5,7]
+let test2 = [10,5,1]
+let test3 = [1,2,3,4,5,6,7,8]
+let test4 = [8,7,6,5,4,3,2,1]
+let test5 = [1,2,3,10,5,6,7,8]
+console.log(makeDec(test1)) //true
+console.log(makeDec(test2)) //false
+console.log(makeDec(test3)) //true
+console.log(makeDec(test4)) //false
+console.log(makeDec(test5)) //true
 
 $(document).ready(function() {
   $('#form-1').submit(function(){
